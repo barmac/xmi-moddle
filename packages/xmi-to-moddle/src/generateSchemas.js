41305a7b49b13e8275fcc809062e88655b93670c
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2023-04-21 15:14:02 +0200

    feat: support more XMI features

--- packages/xmi-to-moddle/src/generateSchemas.js
+++ packages/xmi-to-moddle/src/generateSchemas.js
@@ -1,21 +1,69 @@
 import XmiModdle from 'xmi-moddle';
 
+const PRIMITIVE_TYPES = {
+  'http://www.omg.org/spec/UML/20131001/PrimitiveTypes.xmi#Boolean': 'Boolean',
+  'http://www.omg.org/spec/UML/20131001/PrimitiveTypes.xmi#String': 'String',
+  'http://www.omg.org/spec/UML/20131001/PrimitiveTypes.xmi#Integer': 'Integer'
+};
+const UML_TYPES = {
+  ...PRIMITIVE_TYPES,
+  'http://www.omg.org/spec/UML/20131001/UML.xmi#Element': 'uml:Element'
+};
+
 export async function generateSchemas(xmi) {
   const parseResult = await parse(xmi);
 
-  const { rootElement } = parseResult;
+  const { elementsById, rootElement } = parseResult;
+
+  const context = {
+    elementsById,
+    warnings: []
+  };
 
   const extensions = rootElement.get('extensions');
 
-  // const models = extensions.filter(el => is(el, 'uml:Model'));
-  const packages = extensions.filter(el => is(el, 'uml:Package'));
+  const schemas = extensions.flatMap(
+    extension => handleExtension(extension, context)
+  ).filter(Boolean);
 
-  const schemas = packages.flatMap(handlePackage);
-
-  return schemas;
+  return {
+    schemas,
+    warnings: context.warnings
+  };
 }
 
-function handlePackage(pkg) {
+/**
+ * @param {*} extension
+ * @param {{ elementsById: object }} context
+ */
+function handleExtension(extension, context) {
+  if (is(extension, 'uml:Model')) {
+    return handleModel(extension, context);
+  } else if (is(extension, 'uml:Package')) {
+    return handlePackage(extension, context);
+  }
+
+  return null;
+}
+
+function handleModel(model, context) {
+  const packagedElements = model.get('packagedElement');
+
+  // model is the only package
+  if (packagedElements.some(element => is(element, 'uml:Class'))) {
+    return handlePackage(model, context);
+  }
+
+  return packagedElements.map(element => {
+    if (is(element, 'uml:Package')) {
+      return handlePackage(element, context);
+    }
+
+    return null;
+  });
+}
+
+function handlePackage(pkg, context) {
   const schema = {
     name: pkg.name,
     prefix: pkg.name,
@@ -30,40 +78,66 @@ function handlePackage(pkg) {
 
   const packagedElements = pkg.get('packagedElement');
 
-  packagedElements.forEach(element => handlePackagedElement(element, schema));
+  packagedElements.forEach(element => handlePackagedElement(element, schema, context));
 
   return schema;
 }
 
-function handlePackagedElement(element, schema) {
+function handlePackagedElement(element, schema, context) {
   if (is(element, 'uml:Class')) {
-    handleClass(element, schema);
+    handleClass(element, schema, context);
   }
 }
 
-function handleClass(classElement, schema) {
+function handleClass(classElement, schema, context) {
   const type = {
     name: classElement.name,
     properties: []
   };
 
+  if (classElement.get('isAbstract')) {
+    type.isAbstract = true;
+  }
+
   const attributes = classElement.get('ownedAttribute');
 
-  attributes.forEach(attribute => handleAttribute(attribute, type));
+  attributes.forEach(attribute => handleAttribute(attribute, type, context));
 
   schema.types.push(type);
-
 }
 
-function handleAttribute(attribute, type) {
-  const attributeType = getType(attribute);
-
+function handleAttribute(attribute, moddleType, context) {
   const property = {
-    name: attribute.name,
-    isAttr: isPrimitive(attributeType),
-    isId: attribute.name === 'id',
-    type: attributeType
+    name: attribute.name
   };
+
+  const type = getType(attribute, context);
+
+  if (type.href in UML_TYPES) {
+    property.type = UML_TYPES[type.href];
+  } else if (is(type, 'uml:PrimitiveType')) {
+
+    // moddle does not distinguish String subtypes
+    property.type = 'String';
+  } else if (type.name) {
+    property.type = type.name;
+  } else {
+    context.warnings.push(unknownType(type, attribute, moddleType));
+  }
+
+  if (isPrimitive(property.type)) {
+    property.isAttr = true;
+  }
+
+  // heuristic to detect id
+  if (/id/i.test(property.name)) {
+    property.isId = true;
+  }
+
+  const upperValue = attribute.get('upperValue');
+  if (upperValue && upperValue.get('value') === '*') {
+    property.isMany = true;
+  }
 
   for (const key in property) {
     if (property[key] === false) {
@@ -71,34 +145,18 @@ function handleAttribute(attribute, type) {
     }
   }
 
-  type.properties.push(property);
+  moddleType.properties.push(property);
 }
 
-const PRIMITIVE_TYPES = {
-  'http://www.omg.org/spec/UML/20131001/PrimitiveTypes.xmi#String': 'String',
-  'http://www.omg.org/spec/UML/20131001/PrimitiveTypes.xmi#Integer': 'Integer'
-};
+function getType(attribute, context) {
+  let type = attribute.type || attribute.get('extensions').find(e => e.$type === 'type');
 
-const UML_TYPES = {
-  'http://www.omg.org/spec/UML/20131001/UML.xmi#Element': 'uml:Element'
-};
-
-function getType(attribute) {
-  const type = attribute.type;
-
-  if (type.href && type.href in PRIMITIVE_TYPES) {
-    return PRIMITIVE_TYPES[type.href];
+  const idref = type && type['xmi:idref'];
+  if (idref) {
+    type = context.elementsById[idref];
   }
 
-  if (type.href && type.href in UML_TYPES) {
-    return UML_TYPES[type.href];
-  }
-
-  if (type.idref) {
-    return type.idref.name;
-  }
-
-  throw new Error(`unknown type: ${attribute.type.$type}`);
+  return type;
 }
 
 function isPrimitive(type) {
@@ -113,4 +171,13 @@ function parse(xmi) {
 
 function is(moddleElement, type) {
   return moddleElement.$instanceOf(type);
+}
+
+function unknownType(type, attribute, moddleType) {
+  return {
+    'message': 'unknown type',
+    'class': moddleType.name,
+    'attribute': attribute.name,
+    'type': type
+  };
 }
--- packages/xmi-to-moddle/test/spec/xmi-to-moddle.js
+++ packages/xmi-to-moddle/test/spec/xmi-to-moddle.js
@@ -1,3 +1,5 @@
+import { writeFileSync } from 'node:fs';
+
 import expect from '../expect.js';
 import { readFile } from '../testUtil.js';
 
@@ -12,11 +14,11 @@ describe('xmi-to-moddle', function() {
     const xmi = readFile('test/fixtures/xmi/simple.xmi');
 
     // when
-    const result = await generateSchemas(xmi);
+    const { schemas, warnings } = await generateSchemas(xmi);
 
     // then
-    expect(result).to.exist;
-    expect(result).to.eql([
+    expect(warnings).to.be.empty;
+    expect(schemas).to.eql([
       {
         'name': 'simple',
         'prefix': 'simple',
@@ -54,6 +56,55 @@ describe('xmi-to-moddle', function() {
     const result = await generateSchemas(xmi);
 
     // then
-    expect(result).to.exist;
+    expect(result.schemas).to.exist;
+    expect(result.warnings).to.be.empty;
+  });
+
+
+  it('should generate moddle schema for DMN 1.4', async function() {
+
+    // given
+    const xmi = readFile('test/fixtures/xmi/DMN14.xmi');
+
+    // when
+    const result = await generateSchemas(xmi);
+
+    // then
+    expect(result.schemas).to.exist;
+    expect(result.warnings).to.be.empty;
+  });
+
+
+  it('should generate moddle schema for DMN 1.5', async function() {
+
+    // given
+    const xmi = readFile('test/fixtures/xmi/DMN15.xmi');
+
+    // when
+    const result = await generateSchemas(xmi);
+
+    // then
+    expect(result.schemas).to.exist;
+    expect(result.warnings).to.be.empty;
+  });
+
+
+  it('should generate moddle schema for DMNDI 1.5', async function() {
+
+    // given
+    const xmi = readFile('test/fixtures/xmi/DMNDI15.xmi');
+
+    // when
+    const result = await generateSchemas(xmi);
+
+    // then
+    expect(result.schemas).to.exist;
+    expect(result.warnings).to.be.empty;
   });
 });
+
+
+// eslint-disable-next-line no-unused-vars
+function writeJSON(path, schema) {
+  writeFileSync(path, JSON.stringify(schema, null, 2));
+}
